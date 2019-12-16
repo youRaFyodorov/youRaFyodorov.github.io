2b156c5a3a8afbf8a6a96c3c497f65a02995abab
Yury Fyodorov <fedoruch2000@gmail.com>
2019-12-17 00:49:43 +0300

    site

---
+++ firstsite.js
@@ -0,0 +1,10 @@
+menu.onclick = function Myfunction (){
+    var x = document.getElementById("myTopnav")
+
+    if (x.className === "topnav"){
+        x.className += "responsive";
+    }else {
+        x.className = "topnav"
+    }
+
+}
